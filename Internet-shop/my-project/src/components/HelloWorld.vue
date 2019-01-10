759ea1b3d6e1134c3e4d744fdf6044373bde6754
OlegSozonik <Sozonik999@gmail.com>
2019-01-10 21:09:39 +0300

    feat:create block product and change nav block

--- Internet-shop/my-project/src/components/HelloWorld.vue
+++ Internet-shop/my-project/src/components/HelloWorld.vue
@@ -1,64 +1,534 @@
+
 <template>
-    <div class="navigation-list">
-        <nav class="nav">
-            <div>
-                <a href="">Вход</a>
-            </div>
-            <div>
-                <a href="">Регистрация</a>
-            </div>
-            <div>
-                <a href="">Контакты</a>
-            </div>
-        </nav>
-        <img class="menu-icon" src="">
+  <div class="navigation-list">
+    <nav class="nav">
+      <div>
+        <a href><i class="fas fa-address-card"></i> Registration</a>
+      </div>
+      <div>
+        <a href><i class="fas fa-cart-arrow-down"></i> Card</a>
+      </div>
+    </nav>
+    <div class="packages-list">
+      <div class="package">
+        <img src="../assets/1.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/1.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/5.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/3.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/5.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/1.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/2.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/1.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/4.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/3.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/5.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/2.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/5.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/1.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/3.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
+      <div class="package">
+        <img src="../assets/2.jpg" class="product-image" height="190px" alt="product">
+        <hr>
+        <h5>Apple iPad Mini 2</h5>
+        <div class="package-info-container">
+          <div class="package-info-discription">
+            <p>This item Apple iPad Mini 2 with Retina Display ME276LL/A (16GB, Wi-Fi, Black with Space Gray) (Refurbished)</p>
+          </div>
+          <div class="package-info-line">
+            <p>Sold By:</p>
+            <p>Electronic deals</p>
+          </div>
+          <div class="package-info-line">
+            <p>Price:</p>
+            <p>169$</p>
+          </div>
+        </div>
+        <hr>
+        <div class="package-plan-container">
+          <button>Add to Card</button>
+        </div>
+      </div>
     </div>
+    <div class="fontawesome">
+      <h3>
+        Hello FontAwesome
+        <i class="fas fa-exclamation"/>
+        <i class="fas fa-star"/>
+        <i class="fas fa-star"/>
+        <i class="fas fa-linux"/>
+        <i class="fas fa-cart-arrow-down"></i>
+      </h3>
+    </div>
+  </div>
 </template>
 
 <script>
-
+export default {
+  name: "FontAwesome"
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 * {
-  font-family: 'Roboto', sans-serif;
+  font-family: "Roboto", sans-serif;
+}
+.fontawesome {
+  margin-top: 400px;
 }
 
-.fas {
-  font-size: 130px;
-}
+/* nav */
 
-.fa-igloo {
-  font-size: 40%;
+.fa-star {
+  font-size: 100px;
 }
-.navigation-list {
-    display: flex;
-    justify-content: flex-end;
-    background-color: #1E6FC5;
-}
-
 .nav {
-    display: flex;
-    justify-content: flex-end;
-    width: 53.5%;
-    height: 90px;
-    color: #ffffff;
-    font-family: Lato;
-    font-size: 16px;
-    font-weight: 500;
+  display: flex;
+  justify-content: flex-end;
+  width: 100%;
+  height: 75px;
+  color: #ffffff;
+  font-family: Lato;
+  font-size: 16px;
+  font-weight: 500;
+  background-color: #1e6fc5;
 }
 
 .nav div {
-  margin:30px 30px 0px 0px;
-
+  margin: 30px 30px 0px 0px;
 }
 
-.nav  a {
-  margin:30px 30px 0px 0px;
+.nav a {
+  margin: 30px 30px 0px 0px;
   color: white;
   text-transform: capitalize;
   font-size: 16px;
   font-weight: 300;
+}
+/* nav end */
+
+/* product-block */
+
+.packages-list {
+  margin: 50px auto 0px;
+  width: 75%;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  flex-wrap: wrap;
+}
+
+.package {
+  margin: 20px 0px 20px 0px;
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #ffffff;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.06);
+  border-top: 5px solid #1e6fc5;
+  transition: 1s ease all;
+  cursor: pointer;
+}
+
+.package h5 {
+  color: #474646;
+}
+
+/* package-info-container */
+
+.package-info-container {
+  width: 78.5%;
+  display: flex;
+  flex-direction: column;
+}
+
+.product-image {
+  margin: 10px;
+}
+
+.package-info-line {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  font-family: Lato;
+  font-size: 16px;
+}
+
+.package-info-line > p {
+  margin-top: 6px;
+  margin-bottom: 6px;
+}
+
+/*The end  package-info-container */
+
+.package > h4 {
+  margin-bottom: 0;
+  color: #dfdfdf;
+  font-family: Lato;
+  font-size: 16px;
+  font-weight: 700;
+}
+
+.package > hr {
+  width: 78.5%;
+  height: 1px;
+  background-color: #dfdfdf;
+  border: 0;
+}
+
+.package-plan-container {
+  margin: 10px 0px 20px 0px;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.package-plan-container > button {
+  background-color: #1e6fc5;
+  width: 100%;
+  height: 50px;
+  border-radius: 2px;
+  color: white;
+  text-transform: uppercase;
+  border: 0;
+  -webkit-transition: 0.5s all ease;
+  transition: 0.5s all ease;
+  cursor: pointer;
+}
+
+.package-plan-container > button:hover {
+  background-color: #1e6fc5;
+  box-shadow: 2px 1px 3px grey;
+}
+
+.packages-list {
+  border-color: #1e6fc5;
+}
+
+.package:hover {
+  box-shadow: 0 10px 60px #1e6fc5;
+}
+
+hr {
+  margin: 0px;
 }
 </style>
