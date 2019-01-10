d38c5a102fc3ca41065f4aeedbcc2b2e91f1a2a0
OlegSozonik <Sozonik999@gmail.com>
2019-01-10 03:21:35 +0300

    feat:create Basket component

---
+++ Internet-shop/my-project/src/components/Basket.vue
@@ -0,0 +1,55 @@
+<template>
+<div>
+    <h1>Корзина</h1>
+    <div class="navigation-list">
+    </div>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+* {
+  font-family: 'Roboto', sans-serif;
+}
+
+.fas {
+  font-size: 130px;
+}
+
+.fa-igloo {
+  font-size: 40%;
+}
+.navigation-list {
+    display: flex;
+    justify-content: flex-end;
+    background-color: #1E6FC5;
+}
+
+.nav {
+    display: flex;
+    justify-content: flex-end;
+    width: 53.5%;
+    height: 90px;
+    color: #ffffff;
+    font-family: Lato;
+    font-size: 16px;
+    font-weight: 500;
+}
+
+.nav div {
+  margin:30px 30px 0px 0px;
+
+}
+
+.nav  a {
+  margin:30px 30px 0px 0px;
+  color: white;
+  text-transform: capitalize;
+  font-size: 16px;
+  font-weight: 300;
+}
+</style>
