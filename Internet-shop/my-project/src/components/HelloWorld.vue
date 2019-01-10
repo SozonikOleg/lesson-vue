7a0b966f2b2df6114305aa3219f0022db2815cb2
OlegSozonik <Sozonik999@gmail.com>
2019-01-10 03:19:44 +0300

    feat:create HelloWorld component

--- Internet-shop/my-project/src/components/HelloWorld.vue
+++ Internet-shop/my-project/src/components/HelloWorld.vue
@@ -1,36 +1,64 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-
-  </div>
+    <div class="navigation-list">
+        <nav class="nav">
+            <div>
+                <a href="">Вход</a>
+            </div>
+            <div>
+                <a href="">Регистрация</a>
+            </div>
+            <div>
+                <a href="">Контакты</a>
+            </div>
+        </nav>
+        <img class="menu-icon" src="">
+    </div>
 </template>
 
 <script>
 
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
+* {
+  font-family: 'Roboto', sans-serif;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.fas {
+  font-size: 130px;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.fa-igloo {
+  font-size: 40%;
 }
-a {
-  color: #42b983;
+.navigation-list {
+    display: flex;
+    justify-content: flex-end;
+    background-color: #1E6FC5;
+}
+
+.nav {
+    display: flex;
+    justify-content: flex-end;
+    width: 53.5%;
+    height: 90px;
+    color: #ffffff;
+    font-family: Lato;
+    font-size: 16px;
+    font-weight: 500;
+}
+
+.nav div {
+  margin:30px 30px 0px 0px;
+
+}
+
+.nav  a {
+  margin:30px 30px 0px 0px;
+  color: white;
+  text-transform: capitalize;
+  font-size: 16px;
+  font-weight: 300;
 }
 </style>
